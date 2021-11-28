286a3172a3338c9d9ba6e59c27517a2fdfc256b6
rosadsky <rosadsky@hotmail.com>
2021-11-28 22:07:26 +0100

    fungujuci projekt

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,76 @@ function connectDB() {
             password : 'root',
             database : 'mydb'
         });
+
         connection.connect(err =>{
             if(!err) {
                 connectionMade = true;
             }
         });
+
+        var sql = "CREATE TABLE IF NOT EXISTS Produkt (ID int NOT NULL, Nazov varchar(45) NOT NULL,Image text,Cena float NOT NULL,PRIMARY KEY (ID)) ENGINE=InnoDB ";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+    
+        var sql = "CREATE TABLE IF NOT EXISTS Zakaznik (ID int NOT NULL, email varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,meno varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,ulica varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,cislo varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,mesto varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,psc varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+
+        var sql = "CREATE TABLE IF NOT EXISTS Objednavka (ID int NOT NULL,customer_id int DEFAULT NULL,stav varchar(20) DEFAULT NULL,  PRIMARY KEY (ID),  KEY fk_Order_1_idx (customer_id),  CONSTRAINT fk_Order_1 FOREIGN KEY (customer_id) REFERENCES Zakaznik (ID)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" ;
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+
+        var sql = "CREATE TABLE IF NOT EXISTS ObjednavkaProdukt ( ID int NOT NULL,Order_ID int NOT NULL,Product_ID int NOT NULL,Quantity int NOT NULL,  PRIMARY KEY (ID), KEY fk_OrderItem_1_idx (Order_ID),  KEY fk_OrderItem_2_idx (Product_ID),  CONSTRAINT fk_OrderItem_1 FOREIGN KEY (Order_ID) REFERENCES Objednavka (ID),  CONSTRAINT fk_OrderItem_2 FOREIGN KEY (Product_ID) REFERENCES Produkt (ID)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3"  ;
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+        
+        var sql = "CREATE TABLE IF NOT EXISTS Reklama ( id varchar(100) NOT NULL,  link varchar(255) NOT NULL,image varchar(255) NOT NULL,kliknutia int NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+
+      
+      var sql = "INSERT IGNORE INTO Produkt (ID, Nazov,Image,Cena ) VALUES ?";
+      var values = [
+        [100,'Hnojivo','https://avantgardencentre.co.uk/wp-content/uploads/2018/01/Bonemeal-1-25kg.jpg', 5.99],
+        [101,'Lopata','https://cdn.pixabay.com/photo/2021/03/24/13/46/shovel-6120214_1280.jpg', 25],
+        [102,'Monterky','https://cdn.pixabay.com/photo/2019/09/02/11/07/atlanta-4447010_1280.png', 14.49]];
+
+        connection.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+        });
+        /*
+        var sql = "INSERT IGNORE INTO Reklama (id,link,image,kliknutia) VALUES (?,?,?,?)";
+        connection.query(sql,['reklama','https://pixabay.com/illustrations/black-friday-christmas-1898114/','https://cdn.pixabay.com/photo/2016/12/10/20/23/black-friday-1898114_960_720.jpg',0], function (err, result) {
+          if (err) throw err;
+          console.log(" vlozena reklama");
+        });  */
+        
+        connection.query("SELECT * FROM Reklama LIMIT 50", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+
+            if(result.length === 0){
+                console.log("NIEJE TAM SOMARINA");
+                var sql = "INSERT IGNORE INTO Reklama (id,link,image,kliknutia) VALUES (?,?,?,?)";
+                connection.query(sql,['reklama','https://pixabay.com/illustrations/black-friday-christmas-1898114/','https://cdn.pixabay.com/photo/2016/12/10/20/23/black-friday-1898114_960_720.jpg',0], function (err, result) {
+                if (err) throw err;
+                console.log(" vlozena reklama");
+        }); 
+            }
+            
+        });
+
     }
 }
 
@@ -36,7 +101,7 @@ function getRandomInt(min, max) {
     
     connectDB();
 
-    connection.query("SHOW CREATE TABLE Objednavka", function (err, result, fields) {
+    connection.query("SHOW CREATE TABLE Reklama", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
         res.send(result);
@@ -48,43 +113,12 @@ app.get('/createproducts', function(req, res,next) {
     
     connectDB();
         //"CREATE TABLE `Zakaznik` (\n  `ID` int NOT NULL,\n  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,\n  `meno` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n  `ulica` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n  `cislo` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n  `mesto` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n  `psc` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n  PRIMARY KEY (`ID`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3"}
-    
-        console.log("Connected!");
-        var sql = "CREATE TABLE Produkt (ID int NOT NULL, Nazov varchar(45) NOT NULL,Image text,Cena float NOT NULL,PRIMARY KEY (ID)) ENGINE=InnoDB ";
-        connection.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
-        });
-    
-
-        console.log("Connected!");
-        var sql = "CREATE TABLE Zakaznik (ID int NOT NULL, email varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,meno varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,ulica varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,cislo varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,mesto varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,psc varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3";
-        connection.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
-        });
-
-        console.log("Connected!");
-        //var sql = "" ;
-        connection.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
-        });
-   
-      /*
-      var sql = "INSERT INTO tprodukty ( image,nazov) VALUES ?";
-      var values = [
-        ['image_path_here', 'teplerozky'],
-        ['image_path_here2', 'iPhone'],
-        ['image_path_here3', 'Samsunge']
-        ];
-
-        connection.query(sql, [values], function (err, result) {
+        /*
+        connection.query("CREATE DATABASE mydb", function (err, result) {
             if (err) throw err;
-            console.log("Number of records inserted: " + result.affectedRows);
-        });
-        */
-             
+            console.log("Database created");
+          });
+            */
 
 })
 
